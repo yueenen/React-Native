5c2c32f518a16f257548196799c23034d91bfb88
yueenen <694746075@qq.com>
2017-01-19 17:11:52 +0800

    统一入口页面

--- ListViewDemo/index.ios.js
+++ ListViewDemo/index.ios.js
@@ -4,50 +4,24 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+
+var listViewDemoIndex = require('./listViewDemoIndex.js');
 export default class ListViewDemo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+    render() {
+        return(
+            <View>
+                <listViewDemoIndex/>
+            </View>
+        )
+    }
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('ListViewDemo', () => ListViewDemo);
+AppRegistry.registerComponent('ListViewDemo', () => listViewDemoIndex);
